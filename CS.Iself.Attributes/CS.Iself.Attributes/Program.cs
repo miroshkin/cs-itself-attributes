1e33eb69f2ddde5fff9e028b3b5c65d664f11f5c
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-09 12:38:27 +0300

    Added solution

---
+++ CS.Iself.Attributes/CS.Iself.Attributes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Iself.Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
